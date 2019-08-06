a67ef17cafbb5e80fa2c9f5474b19c7f8f113349
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2019-08-06 17:10:24 -0300

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,50 @@
+    1  ls
+    2  ls lh
+    3  ls -lh
+    4  cls
+    5  cd /
+    6  ls
+    7  cd -
+    8  pwd
+    9  mkdir linuxmint
+   10  ls
+   11  cd linuxmint/
+   12* 
+   13  sudo apt updata && sudo apt install git
+   14  git clone https://github.com/gilsoncaetano/Linuxmint.git
+   15  sudo apt install git
+   16  git clone https://github.com/gilsoncaetano/Linuxmint.git
+   17  ls
+   18  cd Linuxmint/
+   19  echo Perfect k.o
+   20  ls -lh
+   21  echo gilson caetano
+   22  echo gilson caetano > gilson.sh
+   23  ls
+   24  ls -lh
+   25  touch gilson.sh
+   26  ls -lh
+   27  cat LICENSE 
+   28  cat -n LICENSE 
+   29  head LICENSE 
+   30  head -n20 LICENSE 
+   31  tail LICENSE 
+   32  tail -n20 LICENSE 
+   33  less LICENSE 
+   34  cat -n LICENSE | tail
+   35  tail -n100 LICENSE && tail -n200 LICENSE
+   36  head license && tail -n100 LICENSE && tail -n200 LICENSE
+   37  head license tail -n100 LICENSE && tail -n200 LICENSE
+   38  cat -n100 LICENSE
+   39  head LICENSE
+   40  head n100 LICENSE
+   41  tail -n200 LICENSE
+   42  head -n100 LICENSE && less LICENSE && tail -n200 LICENSE
+   43  head -n20 LICENSE
+   44  head LICENSE
+   45  tail -n LICENSE | head -n200 
+   46  cat -n LICENSE | head -n200 
+   47  cat -n LICENSE | head -n200 | tail -n101
+   48  cat -n LICENSE | head -n200 | tail -n101 | less
+   49  history
+   50  history > comandos.sh
