e91c62e96bb36e9fa3dbb406715de45b88050b3c
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2019-08-06 17:12:35 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,85 @@
 Gilson Caetano Aprendendo Github.
+Gilson Caetano Aprendendo  comandos do linux
+
+ comondo
+CTRL - ALT - T abre atalho para o terminal de comandos 
+CTRL - ALT - F1 sai do terminal de comandos 
+
+ls
+ls -lh
+tecla ctrl L
+
+cor de letras de c
+azulr escuro diretorio 
+azulr claro atalho
+verde perigos
+branco arquivos 
+ 
+cd -
+diretorio pessoal
+
+pwd
+verifica qual diretorios eu estou
+
+criação de diretorio 
+mkdir linuxmint
+
+ls confirma se o diretorio foi criado 
+
+tecla tabe completa o comando de 
+
+criar o diretorio linuxmint
+cd linuxmint/ 
+ls confirma se tem conteudo dentro dentro da pasta, e confirma.
+
+Google
+guia foca, guia online
+Versão Iniciante+Intermediário - 5.65 (Abrir em nova janela)
+8.3 pwd
+
+ Mostra o nome e caminho do diretório atual.
+
+Você pode usar o comando pwd para verificar em qual diretório se encontra (caso seu aviso de comandos não mostre isso). 
+
+comando para instala o git que da permição githeb 
+sudo apt updata && sudo apt install git
+
+ls
+cd Linuxmint/
+ls -lh
+
+aparece o texto que voce digito aparece na tela
+echo Perfect k.o
+echo gilson caetano
+
+echo gilson caetano > gilson.sh
+
+ls
+
+lista o conteudo 
+cat -n LICENSE 
+ls -lh
+muda data e hora do arquivo 
+touch gilson.sh
+
+lista o cabesaro do arquivo
+head LICENSE 
+
+comanda de lista as linha do arquivi
+head -n20 LICENSE 
+
+lista o final dor arquivo
+tail LICENSE
+
+lista quantidade de linha que eu quero 
+tail -n20 LICENSE
+
+paginador do livro 
+
+letra Q e para sair da pagina 
+
+ler o conteldo de um a pagina 
+less LICENSE
+
+cat -n LICENSE | head -n200 | tail -n101 | less
+history
